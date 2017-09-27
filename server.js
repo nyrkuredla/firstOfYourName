a852173a9526802253c1bbe65b54aed5d8051fb9
Alonya Lafayette <alonyalafayette@Alonyas-MacBook-Pro.local>
2017-09-27 16:25:22 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+const bodyParser = require('body-parser')
+const compiler = webpack(webpackConfig);
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(webpackDevMiddleware(compiler, {
+  hot: true,
+  filename: 'bundle.js',
+  publicPath: '/',
+  stats: {
+    colors: true,
+  },
+  historyApiFallback: true,
+}));
+
+app.use(bodyParser.json());
+
+
+const server = app.listen(process.env.PORT || 3000, function() {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+module.exports = app;
