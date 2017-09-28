85d5974ed86e03ef09c83c917799b27a6192296b
Brandon Aiton <brandonaiton@icloud.com>
2017-09-28 14:50:42 -0400

    We have dynamic backgrounds  Make sure you adjust your classnames in the variable not the actual main DIVs

--- src/containers/DetailedHousesView.js
+++ src/containers/DetailedHousesView.js
@@ -3,8 +3,9 @@ import React, {Component} from "react"
 
 export default class DetailedHousesView extends Component{
   render(){
+    let className = `hw-100-percent ${this.props.thronepedia}`
     return(
-      <div>
+      <div className={className}>
       </div>
     )
   }
