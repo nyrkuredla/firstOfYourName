331b0e37060e66e7d716125c71631668046ca6db
lotushack <alonyalafayette1@gmail.com>
2017-09-28 16:52:06 -0400

    added TriviaView

--- src/containers/TriviaView.js
+++ src/containers/TriviaView.js
@@ -3,10 +3,19 @@ import React, {Component} from "react"
 
 export default class TriviaView extends Component{
   render(){
-    let className = `hw-100-percent ${this.props.trivia}`
     return(
-      <div className={className}>
+    <div className="game">
+      <div className="questions">
+        <h2>Who said it?</h2>
+        <p>{change_for_quote}</p>
       </div>
+    <form>
+        <button className="answerBtn correctAns" type="submit" value="correct" name="pick">{{answer}}</button>
+    </form>
+       <div>
+        <button className="answerBtn wrongAns" type="radio" value="wrong" name="pick" onclick="{change_wrongAns}">{{answer}}</button>
+       </div>
+     </div>
     )
   }
 }
