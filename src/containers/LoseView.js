1bac08001b15c7feb1c524499af5122c3b8637d9
Ryan Kelley <rtkelley@gmail.com>
2017-09-28 15:05:00 -0400

    loseview now has random death function

--- src/containers/LoseView.js
+++ src/containers/LoseView.js
@@ -1,10 +1,25 @@
-import React, {Component} from "react"
+import React, {Component} from "react";
+import { getRandomDeath } from '../content/deaths'
 
 
 export default class LoseView extends Component{
+  constructor(){
+    super();
+    this.state = {
+      "death": ""
+    }
+  }
+  componentDidMount() {
+    let death = getRandomDeath();
+    this.setState({
+      "death": death
+    })  
+  }
   render(){
+    console.log('state: ', this.state)
     return(
       <div>
+
       </div>
     )
   }
