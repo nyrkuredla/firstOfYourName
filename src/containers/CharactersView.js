07527b1208dea328a92e819df513ec176c2b5f40
Brandon Aiton <brandonaiton@icloud.com>
2017-09-29 09:22:21 -0400

    fixing merge conflicts

--- src/containers/CharactersView.js
+++ src/containers/CharactersView.js
@@ -2,6 +2,12 @@ import React, {Component} from "react"
 
 
 export default class CharactersView extends Component{
+  constructor(){
+    super()
+    this.state = {
+       
+    }
+  }
   render(){
     let className = `hw-100-percent ${this.props.thronepedia}`
     return(
