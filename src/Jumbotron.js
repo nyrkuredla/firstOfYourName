ff3016a1be43346131d3bd8a3b9a62e9aab1ae26
Brandon Aiton <brandonaiton@icloud.com>
2017-09-30 16:36:46 -0400

    Working on the Trivia View now about to make state in the HomeView

--- src/Jumbotron.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react'
-
-const Jumbotron = (props) => {
-  return(
-    <div className="jumbotron text-center">
-      
-    </div>
-  )
-}
--- src/containers/TriviaView.js
+++ src/containers/TriviaView.js
@@ -3,22 +3,31 @@ import React, {Component} from "react"
 import TriviaHeader from "../components/TriviaHeader"
 
 export default class TriviaView extends Component{
+
+_handleChoice = (event) => {
+  console.log("Clicks fired!");
+}
+
   render(){
     let className = `hw-100-percent ${this.props.trivia}`
     return(
     <div className={className}>
       <TriviaHeader/>
-      <div className="questions">
+      <div className="questions parchment">
         <h2>Who said it?</h2>
         <p>change for questions</p>
-      </div>
-    <form>
-        <button className="answerBtn correctAns" type="submit" value="correct" name="pick">Answer</button>
-    </form>
-       <div>
-        <button className="answerBtn wrongAns" type="radio" value="wrong" name="pick" onclick="{change_wrongAns}">Answer</button>
+       <div className="col-6 mx-auto">
+           <div className="input-group">
+             <span className="input-group-addon">
+               <input type="radio" aria-label="Radio button for following text input"/>
+             </span>
+             <label type="text" className="form-control" aria-label="Text input with radio button">
+               Answers will go here!
+             </label>
+           </div>
+         </div>
        </div>
-     </div>
+       </div>
     )
   }
 }
