0073ac67320c7426bb11fe8934d506be07a4d8b7
lotushack <alonyalafayette1@gmail.com>
2017-09-30 12:48:18 -0400

    minor changes to character views

--- src/containers/CharactersView.js
+++ src/containers/CharactersView.js
@@ -19,39 +19,51 @@ export default class CharactersView extends Component{
         <Carousel>
           <div>
             <img src="./bronn.jpg" />
+            <p className="legend">Bronn of the Blackwater</p>
           </div>
           <div>
             <img src="./brynden2.jpg" />
+            <p className="legend">Brynden Tully, the Blackfish</p>
           </div>
           <div>
             <img src="./cersei.jpg" />
+            <p className="legend">Cersei Lannister</p>
           </div>
           <div>
             <img src="./davos.jpg" />
+            <p className="legend">Davos Seaworth, the Onion Knight</p>
           </div>
           <div>
             <img src="./hound.jpg" />
+            <p className="legend">Sandor Clegane, the Hound</p>
           </div>
           <div>
             <img src="./jaime.jpg" />
+            <p className="legend">Jaime Lannister</p>
           </div>
           <div>
             <img src="./Littlefinger.jpg" />
+            <p className="legend">Petyr Baelish</p>
           </div>
           <div>
             <img src="./olenna.jpg" />
+            <p className="legend">Olenna Tyrell, the Queen of Thorns</p>
           </div>
           <div>
             <img src="./Renly.jpg" />
+            <p className="legend">Renly Baratheon</p>
           </div>
           <div>
             <img src="./sansa.jpg" />
+            <p className="legend">Sansa Stark</p>
           </div>
           <div>
             <img src="./tyrion.jpg" />
+            <p className="legend">Tyrion Lannister</p>
           </div>
           <div>
             <img src="./varys.jpg" />
+            <p className="legend">Varys, the Spider</p>
           </div>
         </Carousel>
         </div>
