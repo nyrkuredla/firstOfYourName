9dc7c251d7da2b7f8a0bed6098b4b8b9bb500c76
Ryan Kelley <rtkelley@gmail.com>
2017-09-30 11:54:19 -0400

    forgot to add character functions to services file, done

--- services.js
+++ services.js
@@ -5,6 +5,7 @@ import { fetchRandomQuote, getCorrectAns, getIncorrectAns } from './content/triv
 import { getRandomTitle } from './content/titles'
 import { getRandomDeath } from './content/deaths'
 import { getChosenHouseUrl, getChosenHouseData } from './content/housedata.js'
+import { getChosenCharacterUrl, getChosenCharacterData } from './content/characterdata.js'
 //check to make sure that functions are properly wired through services file to front pages!
 
 //links to APIs
@@ -12,33 +13,4 @@ const quotesLink = "https://got-quotes.herokuapp.com/quotes";
 const apiLink = "https://www.anapioficeandfire.com/api/";
 
 //TO DO:
-//write character data functions (similar to house functions but I don't have the URLs yet)
 //wire up functions to components; this will require setting state for answers, etc. (see individual function files for details on this)
-
-
-//login state:
-constructor(){
-  super();
-
-  this.state = {
-    "name": "",
-    "correctAnsCount": 0,
-    "incorrectAnsCount": 0,
-    "titles": [],
-    "backgroundUrls": []
-  }
-}
-
-  _handleName = (evt) => {
-    let inputName = evt.target.name;
-    this.setState({
-      [inputName] : evt.target.value
-    })
-  }
-
-  _handleSubmit = (evt) => {
-    evt.preventDefault();
-    this.setState(
-      {"name": this.state.name}
-    )
-  }
