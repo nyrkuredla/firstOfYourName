c1d57161649a0f55aa510ac893d023c3ecbc6e79
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-01 12:50:38 -0400

    added some styles to Thronepedianav, but still working on getting it centered

--- src/components/ThronepediaNav.js
+++ src/components/ThronepediaNav.js
@@ -4,8 +4,8 @@ export default class ThronepediaNav extends Component {
   render() {
     return (
 
-<nav className="navbar navbar-expand-lg thronepedia-nav ">
-  <a className="navbar-brand" href="#">Navbar</a>
+<nav className="navbar navbar-expand-lg thronepedia-nav text-justify text-center text-white">
+  <a className="navbar-brand text-justify text-white" href="#">Navbar</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -13,17 +13,17 @@ export default class ThronepediaNav extends Component {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li clasNames="nav-item active">
-        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a className="nav-link text-white text-justify " href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Login</a>
+        <a className="nav-link text-white text-justify" href="#">Login</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Houses</a>
+        <a className="nav-link text-white text-justify" href="#">Houses</a>
       </li>
 
       <li className="nav-item">
-        <a className="nav-link" href="#">Characters</a>
+        <a className="nav-link text-white text-justify" href="#">Characters</a>
       </li>
     </ul>
 
--- src/containers/ThronepediaView.js
+++ src/containers/ThronepediaView.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 
 import ThreeColumns from "../components/ThreeColumns";
 import ThronepediaNav from "../components/ThronepediaNav"
+import '../styles/thronepedianav.css';
 
 import ThronepediaHeader from "../components/ThronepediaHeader"
 export default class ThronepediaView extends Component{
