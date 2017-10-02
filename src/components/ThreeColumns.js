b3c886e0964043cddbfec53ddf430b9129994c4c
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-02 12:19:27 -0400

    making sure all is committed

--- src/components/ThreeColumns.js
+++ src/components/ThreeColumns.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
-// import '../styles/threecolumn.css';
+import '../styles/threecolumn.css';
 
 const ThreeColumns = (props) => {
     return(
       <div className="container w-200 h-200 text-white">
         <div className="row text-white">
-          <div className ="col text-white">
+          <div className ="col text-white house-col">
               <a href="/house">
                 <h3 className="text-white">House</h3>
               </a>
               <img className='house-image w-50 h-50 ' src="./houseTapestry.jpg" alt="picture of the different houses"/>
           </div>
-          <div className ="col text-white">
+          <div className ="col text-white characters-col">
             <a href="/characters">
               <h3 className="text-white">Characters</h3>
             </a>
