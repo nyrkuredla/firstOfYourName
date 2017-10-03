29c4474b8625b7a2f590cf5ef72c2541c20a6a8e
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-03 07:04:14 -0400

    added information in to house and character cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,12 +3,27 @@ import React, { Component } from 'react'
 export default class CharacterCard extends Component {
   render() {
     return (
-      <div class="card">
-        <div class="card-body">
-          This is the character card
+      <div className="card">
+        <div className="card-body">
+          <h4 className="card-title">{this.props.chacterDataObj.name}</h4>
+          <p className="card-text">{this.props.characterDataObj.culture}</p>
+          <p className="card-text">{this.props.characterDataObj.born}</p>
+          <p className="card-text">{this.props.characterDataObj.died}</p>
+          <p className="card-text">{this.props.characterDataObj.titles}</p>
+          <p className="card-text">{this.props.characterDataObj.aliases}</p>
+          <p className="card-text">{this.props.characterDataObj.playedBy}</p>
         </div>
       </div>
     )
   }
 
 }
+// let characterDataObj = {
+//   "name": "",
+//   "culture": "",
+//   "born": "",
+//   "died": "",
+//   "titles": [],
+//   "aliases": [],
+//   "playedBy": []
+// }
--- src/components/HouseCard.js
+++ src/components/HouseCard.js
@@ -4,11 +4,27 @@ export default class CharacterCard extends Component {
   render() {
 
     return (
-    <div class="card">
-      <div class="card-body">
-        This is the house card.
+    <div className="card">
+      <div className="card-body">
+        <h4 className="card-title">{this.props.houseDataObj.name}</h4>
+        <p className="card-text">{this.props.houseDataObj.region}</p>
+        <p className="card-text">{this.props.houseDataObj.coatOfArms}</p>
+        <p className="card-text">{this.props.houseDataObj.words}</p>
+        <p className="card-text">{this.props.houseDataObj.titles}</p>
+        <p className="card-text">{this.props.houseDataObj.seats}</p>
+        <p className="card-text">{this.props.houseDataObj.ancestralWeapons}</p>
       </div>
     </div>
   }
 
 }
+
+// let houseDataObj = {
+//   "name": "",
+//   "region": "",
+//   "coatOfArms": "",
+//   "words": "",
+//   "titles": [],
+//   "seats": [],
+//   "ancestralWeapons": []
+// }
